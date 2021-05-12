8aa408cba9deeed5f60ae33a82c048af1f338c5f
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-12 17:32:45 +0530

    [Amulya] added attendance code

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,5 +3,7 @@ package com.employeewage;
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		double attendance=Math.floor(Math.random()*2);
+		System.out.println(attendance);
 	}
 }
