74a93dea874963d1ad52ce0a21182b9227570c45
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-12 17:56:39 +0530

    [Amulya] added daily wage of an employee

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,7 +3,16 @@ package com.employeewage;
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int wageperhr=20;
+		int dailyhrs=8;
+		int dailywage;
+		dailywage=wageperhr*dailyhrs;
+		System.out.println("Daily wage of the employee = "+dailywage);
 		double attendance=Math.floor(Math.random()*2);
-		System.out.println(attendance);
+		if(attendance==1.0) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
 	}
 }
